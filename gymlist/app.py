9c60aa6cb03c8d34bd0276f2472a67a5fda3676e
JunyoungChoi <42435823+JunyoungChoi2020@users.noreply.github.com>
2022-09-22 17:53:03 +0900

    FirstWeek

---
+++ gymlist/app.py
@@ -0,0 +1,202 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+import json
+from datetime import datetime, timedelta
+import hashlib
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+client = MongoClient('mongodb+srv://JYChoi:wnsdn123@cluster0.uzxmk.mongodb.net/?retryWrites=true&w=majority')
+db = client.gyms
+posts = db.mypost
+user = db.users
+dajim = db.dajim
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('index.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+@app.route('/api/posts/upload', methods=["POST"])
+def save_gym():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
+        gym_info = json.loads(request.form['doc'])
+        doc = {
+            "place_name": gym_info['place_name'],
+            "address": gym_info['address'],
+            "id": gym_info['id'],
+            "phone": gym_info['phone'],
+            "place_url": gym_info['place_url'],
+            "road_address": gym_info['road_address'],
+            "x": gym_info['x'],
+            "y": gym_info['y'],
+            "image_url": gym_info['image_url'],
+            "user_id": username
+        }
+        posts.insert_one(doc)
+        return jsonify({"result":"success", 'msg': "찜 리스트에 추가하였습니다."})
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/api/posts/delete', methods=["POST"])
+def delete_gym():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload["id"]
+        gym_info = json.loads(request.form['doc'])
+        doc = {
+            "place_name": gym_info['place_name'],
+            "address": gym_info['address'],
+            "id": gym_info['id'],
+            "phone": gym_info['phone'],
+            "place_url": gym_info['place_url'],
+            "road_address": gym_info['road_address'],
+            "x": gym_info['x'],
+            "y": gym_info['y'],
+            "image_url": gym_info['image_url'],
+            "user_id": username
+        }
+        posts.delete_one(doc)
+        return jsonify({"result": "success", 'msg': "성공적으로 삭제되었습니다."})
+    except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/main', methods=["GET"])
+def main():
+    return render_template("index.html")
+
+@app.route('/api/posts/result', methods=["GET"])
+def mypost_results():
+    all_post = posts.find({});
+    return jsonify(all_post)
+
+@app.route('/api/posts/post_exist_check', methods=["POST"])
+def check_post_exist():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        username = payload['id']
+        docs = json.loads(request.form['gymsInfoList'])
+        all_post = posts.find({"user_id": username})
+        check_list = [False] * len(docs)
+
+        for post in all_post:
+            counter = 0
+            for doc in docs:
+                if (doc["place_name"] == post["place_name"]) and (doc["address"] == post["address"]):
+                    check_list[counter] = True
+                    break
+
+                counter += 1
+
+        return jsonify({"result": "success", "checkList": check_list})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+@app.route('/api/user/login', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+@app.route('/api/dajim/upload', methods=['POST'])
+def save_dajim():
+    content_receive = request.form['content_give']
+
+    doc = {
+        'content': content_receive
+    }
+
+    db.dajim.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': "오늘의 다짐이 등록되었습니다."})
+@app.route('/api/dajim/show', methods=['GET'])
+def show_dajim():
+    query = {'$sample': {'size': 1}}
+    cursor = list(db.dajim.aggregate([query]))[0]['content']
+    print(cursor)
+    return jsonify({'data': cursor})
+
+@app.route('/api/user/signup', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    # nickname_receive = request.form['nickname_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        # "nickname": nickname_receive,                               # 닉네임
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+@app.route('/api/user/signup/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    # nickname_receive = request.form['nickname_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/mypage', methods=["GET"])
+def mypage():
+    return render_template('mypage.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
